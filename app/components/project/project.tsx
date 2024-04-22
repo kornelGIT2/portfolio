61cb058b04ed7d16a3299cad1657710db4db49fe
Kornel <uzarowskik@gmail.com>
2024-04-22 14:23:09 +0200

    project section fix for mobile

--- app/components/project/project.tsx
+++ app/components/project/project.tsx
@@ -22,7 +22,7 @@ function Project({
       className=" from-slate-300 to-slate-100 group max-w-[80rem] max-h-[25rem] w-full h-full -z-[0] 
       overflow-hidden rounded-xl relative dark:hover:to-slate-950 dark:hover:from-slate-800 hover:to-slate-100 hover:from-slate-200 transition sm:pr-8  "
     >
-      <div className="py-4 md:space-y-8  space-y-1 md:pb-7 sm:px-5 md:pt-10 lg:max-w-[50%]  flex flex-col h-full justify-center lg:group-even:items-end lg:group-odd:items-start lg:group-even:text-end lg:group-odd:text-start items-center lg:group-even:ml-[40rem]">
+      <div className="py-4 md:space-y-8  space-y-4 md:pb-7 sm:px-5 md:pt-10 lg:max-w-[50%]  flex flex-col h-full justify-center lg:group-even:items-end lg:group-odd:items-start lg:group-even:text-end lg:group-odd:text-start items-center lg:group-even:ml-[40rem]">
         <a
           href={sourceCode}
           className="sm:text-2xl text-lg font-semibold dark:text-white text-slate-800"
@@ -38,7 +38,7 @@ function Project({
             return (
               <li
                 key={tag.name}
-                className="px-3  text-[0.7rem] bg-slate-300 dark:bg-slate-700 uppercase tracking-wider  rounded-full  flex  items-center justify-center p-2"
+                className="px-3  text-[0.5rem] md:text-[0.7rem] bg-slate-300 dark:bg-slate-700 uppercase tracking-wider  rounded-full  flex  items-center justify-center p-2"
               >
                 {/* <Image
                   src={tag.icon}
@@ -51,7 +51,7 @@ function Project({
                       : ""
                   }`}
                 /> */}
-                <span className="dark:text-slate-300 font-medium text-slate-700 ">
+                <span className="dark:text-slate-300 font-semibold text-slate-700 ">
                   {tag.name}
                 </span>
               </li>
@@ -59,7 +59,7 @@ function Project({
           })}
         </ul>
 
-        <div className="flex space-x-4 ">
+        <div className="md:flex space-x-4 hidden">
           <a href={sourceCode}>
             <Image
               src="/github3.svg"
